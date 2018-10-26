b9b7e5ebe9aa3ea71427efbdd9380862b60e06bb
simon8029 <simon.lau@msn.com>
2018-10-25 23:17:48 -0400

    Add Table component.

---
+++ Table/Table.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Table extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+	render() {
+		return <div>Table Works!</div>;
+	}
+}
+
+export default Table;
