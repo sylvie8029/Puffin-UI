e96e01422cd280045d315927b269d64ebea4fe8c
simon8029 <simon.lau@msn.com>
2018-12-30 21:31:09 -0500

    Carousel Component - events.

--- Carousel/index.js
+++ Carousel/index.js
@@ -1,7 +1,51 @@
 class Carousel extends HTMLElement {
-	connectedCallback() {}
+	connectedCallback() {
+		this._imageIndex = 0;
+		this._images = this.getAttribute('images').split(',');
+
+		this.innerHTML = `
+		<h2>${this.getAttribute('title')}</h2>
+
+		<h4>by ${this.getAttribute('desc')}</h4>
+
+		<div class="image-container"></div>
+
+		<button class="back">&lt</button>
+		
+		<button class="forward">&gt</button>`;
+
+		this.showImage();
+
+		this.querySelector('button.back').addEventListener('click', event =>
+			this.onBackButtonClick(event),
+		);
+		this.querySelector('button.forward').addEventListener('click', event =>
+			this.onForwardButtonClick(event),
+		);
+	}
+
+	showImage() {
+		this.querySelector('.image-container').style.backgroundImage =
+			'url(' + this._images[this._imageIndex] + ')';
+	}
+
+	onBackButtonClick(event) {
+		this._imageIndex--;
+		if (this._imageIndex < 0) {
+			this._imageIndex = this._images.length - 1;
+		}
+		this.showImage();
+	}
+
+	onForwardButtonClick(event) {
+		this._imageIndex++;
+		if (this._imageIndex >= this._images.length) {
+			this._imageIndex = 0;
+		}
+		this.showImage();
+	}
 }
 
-if (!customElements.get('carousel')) {
-	customElements.define('carousel', Carousel);
+if (!customElements.get('p-carousel')) {
+	customElements.define('p-carousel', Carousel);
 }
